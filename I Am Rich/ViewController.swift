bf5f6519e5342910ed97a9ee7ae6f56bf910c1f2
Yixing-Lu <yixinglu97@gmail.com>
2020-06-29 14:39:34 -0700

    first commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Yixing Lu on 6/29/20.
+//  Copyright © 2020 Yixing Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
